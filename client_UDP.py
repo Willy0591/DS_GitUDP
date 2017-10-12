320d730814e796dc1cc7654201c09366585cbab4
Willy0591 <pernez@lacroixrouge-brest.fr>
2017-10-12 15:14:21 +0200

    envoi et reponse de la trame

--- client_UDP.py
+++ client_UDP.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 
-import socket
-
-
+import socket#Importé la biblio socket
+UDP_IP= "192.168.0.202" #Adresse IP du serveur 
+UDPport= 5005 #Port utilisé pour la communication
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.settimeout(1.0)
+sock.sendto("cinema",(UDP_IP,UDPport))#Envoi de la trame  
 
-sock.connect(???)
-sock.send(???)
+trameReponse, addr = sock.recvfrom(1024)#Récuperation de la trame
 
+print "Réception de la trame de réponse", trameReponse.encode("hex")#Affichage de la trame
 
-trameReponse, addr = sock.recvfrom(1024)
+code1=int(ord(trameReponse[0])<<24)
+code2=code1+(ord(trameReponse[1])<<16)
+code3=code2+(ord(trameReponse[2])<<8)
+code4=code3+(ord(trameReponce[3])
 
-print "Réception de la trame de réponse", trameReponse.encode("hex")
+code = ord()#transformation de la trame en code 
 
-code = ???
-
-print code
+print code #Affichage du code 
 
